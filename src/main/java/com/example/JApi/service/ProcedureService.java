03c04fdb5ab9b18bdd38eaa9e78be26ee6b0e83f
Maksim <email@mail.ru>
2023-05-17 18:35:29 +0400

    JA-9 ProcedureService  комментарии

--- src/main/java/com/example/JApi/service/ProcedureService.java
+++ src/main/java/com/example/JApi/service/ProcedureService.java
@@ -20,32 +20,49 @@ public class ProcedureService {
         this.procedureConfig = procedureConfig;
     }
 
+    /**
+     * Функция старта вызова хранимой процедуры и вызова API
+     * @param name - имя функции
+     * @param body - параметры
+     * @return Результат функции Map<String, Object> или List<Map<String, Object>>
+     */
     public Object procedure(String name, Map<String, Object> body) throws Exception {
-        Map<String, Connection> connectionMap = connect.getConnection();
-        ProceduresConfig proceduresConfig = procedureConfig.getProcedure().get(name);
-        Connection connectionInstance = connectionMap.get(proceduresConfig.getConnectString());
+        Map<String, Connection> connectionMap = connect.getConnection(); //  список подключении
+        ProceduresConfig proceduresConfig = procedureConfig.getProcedure().get(name); // получить функцию конфиг
 
-        if(connectionInstance == null){
+        if(proceduresConfig == null){ // проверка наличия функции
+            throw new Exception("Функция с именем " + name + " не найдена");
+        }
+
+        Connection connectionInstance = connectionMap.get(proceduresConfig.getConnectString()); // необходимое соединение
+
+        if(connectionInstance == null){ // если соединения не существует
             throw new Exception("Соединение с именем " + proceduresConfig.getConnectString() + " не найдено");
         }
 
+        // получение результата из функции
         List<Map<String, Object>> procedureSqlResult = procedureSql(connectionInstance, procedureConfig.getProcedure().get(name), body);
 
-        if(!proceduresConfig.isArray()){
+        if(!proceduresConfig.isArray()){ // функция возвращает не массив
             return  procedureSqlResult.get(0);
         }
 
         return procedureSqlResult;
     }
 
+    /**
+     * Функция парсит ResultSet в List<Map<String, Object>>
+     * @param resultSet - результат функции бд
+     * @return List<Map<String, Object>>
+     */
     private List<Map<String, Object>> parsingResultSet(ResultSet resultSet) throws SQLException {
         List<Map<String, Object>> result = new ArrayList<>();
 
-        while (resultSet.next()){
-            ResultSetMetaData metaData = resultSet.getMetaData();
+        while (resultSet.next()){ // данные от хранимой процедуры
+            ResultSetMetaData metaData = resultSet.getMetaData(); // получить методанные
             Map<String, Object> map = new HashMap<>();
 
-            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+            for (int i = 1; i <= metaData.getColumnCount(); i++) { // прогнать методанные колонки
                 map.put(
                         metaData.getColumnName(i),
                         this.saveTypeParsing(resultSet, metaData, i)
@@ -58,16 +75,28 @@ public class ProcedureService {
         return result;
     }
 
+    /**
+     * Функция возвращает значение с ResultSet с необходимым типом
+     * @param resultSet - ответ хранимой процедуры
+     * @param metaData - методанные
+     * @param index - index нужен для возвращение значения
+     */
     private Object saveTypeParsing(ResultSet resultSet, ResultSetMetaData metaData, int index) throws SQLException {
         return switch (metaData.getColumnType(index)) {
             case Types.VARCHAR, Types.CHAR, Types.LONGVARCHAR -> resultSet.getString(index);
             case Types.INTEGER, Types.BIGINT -> resultSet.getInt(index);
-            case Types.STRUCT -> throw new Error("Структура не поддерживается");
             case Types.DATE -> resultSet.getDate(index);
+            case Types.STRUCT -> throw new Error("Структура не поддерживается");
             default -> throw new Error("Указанный тип данных не обрабатывается!");
         };
     }
 
+    /**
+     *  Функция меняет ? в хранимой процедуре на соотвествующие значение по конфигу proceduresConfig и параметрам body
+     * @param preparedStatement - объект вызова храминой процедуры
+     * @param body - параметры хранимой процедуры от API
+     * @param proceduresConfig - конфиг хранимой процедуры
+     */
     private void saveParameters(PreparedStatement preparedStatement, Map<String, Object> body, ProceduresConfig proceduresConfig){
         proceduresConfig.getProceduresParametersConfigList().forEach(proceduresParametersConfig -> {
 
@@ -98,6 +127,13 @@ public class ProcedureService {
         });
     }
 
+    /**
+     * Вызов хранимой процедуры после валидации
+     * @param connectionInstance - соединение к бд
+     * @param proceduresConfig - конфиг храминой процедур
+     * @param body - параметры храминой процедур от API
+     * @return List<Map<String, Object>>
+     */
     public List<Map<String, Object>> procedureSql(
             Connection connectionInstance,
             ProceduresConfig proceduresConfig,
